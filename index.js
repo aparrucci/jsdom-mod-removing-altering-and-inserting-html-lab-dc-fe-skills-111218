6b4d9d911250a8dffdb0682b6c77dc38f9df745d
Andrew Parrucci <am.parrucci@gmail.com>
2019-01-08 00:07:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ main.remove();
 
 /* Create your new element here and assign it to newHeader */
 
-var newHeader = document.createElement(H1);
+var newHeader = document.createElement('H1');
 document.body.appendChild(newHeader);
 newHeader.id = 'victory';
 newHeader.innerHTML = "Andrew is the champion";
